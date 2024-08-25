ab26d6f96b380886fbfe4509d566afaa1f9a7786
MiqueiasLima <miqueiaslima@outlook.com>
2024-08-24 23:26:58 -0300

    Set:HashSet: Pesquisa em HashSet

---
+++ src/set/pesquisa/teste/AgendaContatosTeste.java
@@ -0,0 +1,27 @@
+package set.pesquisa.teste;
+
+import set.pesquisa.dominio.AgendaContatos;
+
+public class AgendaContatosTeste {
+    public static void main(String[] args) {
+
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Maria"));
+
+        System.out.println("Contato Atualizado: "+agendaContatos.atualizarNumeroContato("Maria Fernandes",11111));
+
+        agendaContatos.exibirContatos();
+    }
+}
